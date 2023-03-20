b638630d1d1838afbaa045da45a1d4c5349bb902
Aleksandra0001 <104300297+Aleksandra0001@users.noreply.github.com>
2023-03-20 20:57:12 +0200

    fix dev

--- recognizer/main/views.py
+++ recognizer/main/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+
+
 # from newsapp.views import get_news
 # from book_app.views import main as phonebook_main
 # from note_app.views import main as note_main
@@ -7,23 +9,27 @@ from django.shortcuts import render, redirect
 
 
 # # Create your views here.
-# @login_required
+@login_required
 def homepage(request):
     # return render(request, "main/homePage.html")
     return render(request, "main/galleryPage.html")
     # return redirect(get_news)
+
+
 #
 #
-# @login_required
-# def phonebook_page(request):
-#     # return render(request, "main/phoneBookPage.html")
-#     return redirect(phonebook_main)
+@login_required
+def phonebook_page(request):
+    # return render(request, "main/phoneBookPage.html")
+    return render(request, "main/galleryPage.html")
+
+
 #
 #
-# @login_required
-# def notebook_page(request):
-#     # return render(request, "main/noteBookPage.html")
-#     return redirect(note_main)
+@login_required
+def notebook_page(request):
+    # return render(request, "main/noteBookPage.html")
+    return render(request, "main/galleryPage.html")
 
 
 @login_required
