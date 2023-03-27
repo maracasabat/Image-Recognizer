8cb5e1195d534338222eeb2aa9b5d7a8bdcd46bd
Ivan Yevpat <maracasabat@gmail.com>
2023-03-28 00:16:33 +0300

    added cloudinary storage

--- recognizer/mediauploadapp/models.py
+++ recognizer/mediauploadapp/models.py
@@ -2,13 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import FileExtensionValidator
 from django.contrib.auth import get_user_model
+import cloudinary.uploader
 
 
 class Photo(models.Model):
     title = models.CharField(max_length=255, blank=True)
-    file = models.FileField(upload_to='photos/',
-                            validators=[FileExtensionValidator(allowed_extensions=['jpg', 'png', 'svg', 'jpeg', 'gif', 'eps', 'raw'])],
-                            unique=True)
+    file = models.ImageField(upload_to='photos/',
+                             validators=[FileExtensionValidator(
+                                 allowed_extensions=['jpg', 'png', 'svg', 'jpeg', 'gif', 'eps', 'raw'])],
+                             unique=True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
     author_id = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, null=True)
 
